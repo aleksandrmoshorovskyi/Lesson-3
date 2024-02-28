89c564e67a0380f8af343543f913469b241f5c2c
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-02-28 13:02:33 +0200

    for

--- Lesson-3.playground/Contents.swift
+++ Lesson-3.playground/Contents.swift
@@ -1,1 +1,10 @@
-//
+import UIKit
+
+//Потік керування
+//Цикл For
+
+let products: [String] = ["Молоко", "Печиво", "Кава", "Апельсини", "Яблука"]
+
+for index in 0 ..< products.count {
+    print(products[index])
+}
